4a5545f8be9eeeef2cfc2b4c540e547197224f07
Hossam Mahmoud <hossam.uddin1@gmail.com>
2025-09-13 16:00:23 +0300

    changing the source data and restructuring some files

--- TELECOM_SSIS/SCRIPTS/04_create_err_source_output.sql
+++ TELECOM_SSIS/SCRIPTS/04_create_err_source_output.sql
@@ -1,5 +1,16 @@
-use EO_Telecom_GrgEdu
-go
+
+/*==============================================================
+ Author		: Hossam Mahmoud
+ Purpose	: Create error for source output table
+ Date			: 2025-08-11
+ Notes		: 
+			- Drops the table if it exists
+			- Creates a table as required
+==============================================================*/
+
+-- Switch context to the target database
+USE HM_STAG_ETL_DB;
+GO
 
 CREATE TABLE err_source_output (
 	id int identity(1,1),
